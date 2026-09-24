7810bf3929bb06e80dcec35a867f1bc55a0b4183
anglesgirl <anglesgirl@github.com>
2026-09-24 04:22:31 +0000

    fix: 补齐 ECH SDK 配置导入

--- core/common/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ core/common/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.network
 
 import android.content.Context
+import com.anglesgirl.echsdk.EchSdk
 import eu.kanade.tachiyomi.network.interceptor.CloudflareInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UncaughtExceptionInterceptor
 import eu.kanade.tachiyomi.network.interceptor.UserAgentInterceptor
