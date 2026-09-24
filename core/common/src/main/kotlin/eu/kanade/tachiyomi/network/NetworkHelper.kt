8bc3935146bf7e8f15f8d73050d395fee0fadabd
anglesgirl <anglesgirl@github.com>
2026-09-24 04:40:47 +0000

    fix: 避免主线程重复初始化 ECH SDK

--- core/common/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ core/common/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -57,7 +57,6 @@ class NetworkHelper(
             else -> builder
         }
 
-        EchSdkState.install(context)
         if (EchSdkState.enabled) builder.also(EchSdk::configure)
         builder
     }
